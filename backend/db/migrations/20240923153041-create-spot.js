5be1d44d3483bc2c19cb78cca71c83752c795f34
aechon <tony.hronowski@gmail.com>
2024-09-26 17:19:39 -0400

    typo

--- backend/db/migrations/20240923153041-create-spot.js
+++ backend/db/migrations/20240923153041-create-spot.js
@@ -20,7 +20,7 @@ module.exports = {
         allowNull: false,
         reference: {
           model: 'Users',
-          key: id
+          key: 'id'
         },
         onDelete: 'CASCADE'
       },
