86c5f30373c63b7f7842254ea24edd3ab389c4b7
aechon <tony.hronowski@gmail.com>
2024-10-19 14:08:57 -0400

    added additional seed data for testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
-// backend/routes/index.js
-// ...
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-// ...
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken(); // Generate a new CSRF token
